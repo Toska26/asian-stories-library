f04c18b59f67a43a69e9ce87d31aa6460c691405
Toska26 <bruna1974@gmail.com>
2026-09-11 19:38:02 +0200

    Add DRAMAS.js with initial drama entries

---
+++ datos/DRAMAS.js
@@ -0,0 +1,21 @@
+const DRAMAS = [
+
+    {
+        codigo: 'DR000031',
+        titulo: 'Remember Me',
+        activo: true
+    },
+
+    {
+        codigo: 'DR000032',
+        titulo: 'Be Mine, Superstar',
+        activo: true
+    },
+
+    {
+        codigo: 'DR000033',
+        titulo: 'Police in Love',
+        activo: true
+    }
+
+];
