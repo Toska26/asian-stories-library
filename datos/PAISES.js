5fa74dd63eb2db4cc03e262f4167b8c57658638e
Toska26 <bruna1974@gmail.com>
2026-09-11 22:47:31 +0200

    Fix South Korea flag emoji in PAISES.js

--- datos/PAISES.js
+++ datos/PAISES.js
@@ -7,7 +7,7 @@ const PAISES = [
     { codigo: 'HK', nombre: 'Hong Kong', alias: [ 'Hong Kong', '香港' ], bandera: '🇭🇰', activo: true  },
     { codigo: 'ID', nombre: 'Indonesia', alias: [ 'Indonesia' ], bandera: '🇮🇩', activo: true },
     { codigo: 'JP', nombre: 'Japón', alias: [ 'Japan', '日本' ], bandera: '🇯🇵', activo: true },
-    { codigo: 'KR', nombre: 'Corea del Sur', alias: [ 'South Korea', 'Korea',  '대한민국' ], bandera: '🇨🇳', activo: true },
+    { codigo: 'KR', nombre: 'Corea del Sur', alias: [ 'South Korea', 'Korea',  '대한민국' ], bandera: '🇰🇷', activo: true },
     { codigo: 'MO', nombre: 'Macao', alias: [  'Macau',  '澳門'  ], bandera: '🇲🇴', activo: true },
     { codigo: 'MY', nombre: 'Malasia', alias: [  'Malaysia' ], bandera: '🇲🇾', activo: true },
     { codigo: 'PH', nombre: 'Filipinas', alias: [ 'Philippines', 'Pilipinas'  ], bandera: '🇵🇭', activo: true },
