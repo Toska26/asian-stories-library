34e2c972480634f4863f02cf27fd94ee87f5fa2b
Toska26 <bruna1974@gmail.com>
2026-09-18 18:54:55 +0200

    Actualizar DRAMAS.js

--- datos/DRAMAS.js
+++ datos/DRAMAS.js
@@ -1977,9 +1977,7 @@ const DRAMAS = [
     sinopsis: 'Phugun es un chico ordinario que nunca ha tenido una relación amorosa ni cree en el amor verdadero. Todo cambia cuando Cirrus, un apuesto y misterioso hombre, aparece en su vida afirmando venir de un mundo paralelo donde ambos son pareja. Aunque Phugun al principio se muestra escéptico, la devoción y protección incondicional de Cirrus comienzan a derribar sus barreras, obligándolo a cuestionar sus sentimientos y la realidad de los universos paralelos.',
         multimedia: {
         portada: [
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No_image_available.svg/1024px-No_image_available.svg.png',
-            'https://image.tmdb.org/t/p/original/a8y1qN6b438z35Wj9sY1.jpg',
-            'https://yflix.me/wp-content/uploads/2025/01/The-Boy-Next-World.jpg'
+            'https://wsrv.nl/?url=https://image.tmdb.org/t/p/original/a8y1qN6b438z35Wj9sY1.jpg'
         ],
         trailer: ['https://www.youtube.com/watch?v=Iw5dFkxvtMU'],
         teaser: ['https://www.youtube.com/watch?v=hLyjZGz0-Cc'],
