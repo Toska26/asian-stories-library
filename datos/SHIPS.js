1ad87748d09c6cd8f1a366251419a8bad37f268d
Toska26 <bruna1974@gmail.com>
2026-09-19 12:35:53 +0200

    Fix 'personas' for FirstKhaotung in SHIPS.js
    Updated the 'personas' array for 'FirstKhaotung' to replace 'PR000XXX' with 'PR000017'.

--- datos/SHIPS.js
+++ datos/SHIPS.js
@@ -71,8 +71,8 @@ const SHIPS = [
    {codigo:'SH000065', nombre:'MilkNewyear', personas:['PR000366','PR000367'], activo:true, oficial:true},
    {codigo:'SH000066', nombre:'PopTitle', personas:['PR000368','PR000250'], activo:true, oficial:true},
    {codigo:'SH000067', nombre:'OttoBJ', personas:['PR000369','PR000370'], activo:true, oficial:true},
-
-   {codigo:'SH000068', nombre:'FirstKhaotung', personas:['PR000388','PR000XXX'], activo:true, oficial:true},
+   
+   {codigo:'SH000068', nombre:'FirstKhaotung', personas:['PR000388','PR000017'], activo:true, oficial:true},
    {codigo:'SH000069', nombre:'KengNamping', personas:['PR000394','PR000393'], activo:true, oficial:true},
    {codigo:'SH000070', nombre:'TleFirstOne', personas:['PR000395','PR000396'], activo:true, oficial:true},
 ];
