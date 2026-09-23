7ea7e40b9d08f3a6398bbba376f49bc06436450a
Toska26 <bruna1974@gmail.com>
2026-09-23 17:18:42 +0200

    Add NeoLouis ship entry to SHIPS.js

--- datos/SHIPS.js
+++ datos/SHIPS.js
@@ -123,4 +123,5 @@ const SHIPS = [
    {codigo:'SH000109', nombre:'FlukeOhm', personas:['PR000557','PR000558'], activo:true, oficial:true}, 
    {codigo:'SH000110', nombre:'GreatBright', personas:['PR000559','PR000390'], activo:true, oficial:true},
    {codigo:'SH000111', nombre:'Mek & Mark', personas:['PR000270','PR000530'], activo:false, oficial:false},
+   {codigo:'SH000112', nombre:'NeoLouis', personas:['PR000294','PR000560'], activo:false, oficial:true},
 ];
