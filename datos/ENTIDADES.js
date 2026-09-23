9df9578d0e1bcac6152d45477f858014c4aafba7
Toska26 <bruna1974@gmail.com>
2026-09-23 13:36:37 +0200

    Update ENTIDADES.js

--- datos/ENTIDADES.js
+++ datos/ENTIDADES.js
@@ -53,5 +53,6 @@ const ENTIDADES = [
 { codigo: 'EN000047', nombre: 'Domundi TV', pais: 'TH', tipos: ['Productora'], activo: true },
 { codigo: 'EN000048', nombre: 'Copy A Bangkok', pais: 'TH', tipos: ['Productora'], activo: true },
    { codigo: 'EN000049', nombre: 'TrueVisions Now', pais: 'TH', tipos: ['Emisora'], activo: true },
+{codigo:'EN000050', nombre:'GMO Films', pais:'TH', tipos:['Productora'], activo:true},
 
 ];
