551f6318b7bac520e61af228ceadf65fc151ba53
Toska26 <bruna1974@gmail.com>
2026-09-24 23:13:27 +0200

    Add new universe entry for Kim Ji-woong & Yoon Seo-bin

--- datos/UNIVERSOS.js
+++ datos/UNIVERSOS.js
@@ -3,5 +3,6 @@
 ====================================================== */  
 const UNIVERSOS = [
   { codigo:'UN000001', nombre:'MAME', descripcion:'Universo de historias y personajes relacionados con las obras de MAME.', activo:true },
-  { codigo:'UN000002', nombre:'VBL Series', descripcion:'Colección de series BL taiwanesas formada por Stay by My Side, You Are Mine, VIP Only y AntiReset', activo:true }
+  { codigo:'UN000002', nombre:'VBL Series', descripcion:'Colección de series BL taiwanesas formada por Stay by My Side, You Are Mine, VIP Only y AntiReset', activo:true },
+    { codigo:'UN000003', nombre:'Kim Ji-woong & Yoon Seo-bin', descripcion:'Universo de producciones BL en las que Kim Ji-woong y Yoon Seo-bin interpretan juntos a la pareja protagonista.', activo:true }
 ];
