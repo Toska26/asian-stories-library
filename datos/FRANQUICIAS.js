16cea1240b2b1d4da5bf765cb9ec0006f65be1e7
Toska26 <bruna1974@gmail.com>
2026-09-25 19:19:27 +0200

    Update FRANQUICIAS.js

--- datos/FRANQUICIAS.js
+++ datos/FRANQUICIAS.js
@@ -14,5 +14,5 @@ const FRANQUICIAS = [
    {codigo:'FR000012', nombre:'Bed Friend', pais:'TH', multimedia:{portada:'FR000012.webp'}, descripcion:'Franquicia tailandesa formada por Bed Friend y su spin-off The Middleman’s Love.', activo:true},
    {codigo:'FR000013', nombre:'Star & Sky', pais:'TH', multimedia:{portada:''}, descripcion:'Franquicia tailandesa formada por Star in My Mind y Sky in Your Heart.', activo:true},
    {codigo:'FR000014', nombre:'A Breeze of Love', pais:'KR', multimedia:{portada:'FR000014.webp'}, descripcion:'Franquicia BL surcoreana formada por A Breeze of Love y su spin-off Always Meet Again.', activo:true},
-
+    {codigo:'FR000015', nombre:'Unintentional Love Story', pais:'KR', multimedia:{portada:''}, descripcion:'Franquicia BL surcoreana formada por Unintentional Love Story y su spin-off The Time of Fever.', activo:true},
 ];
